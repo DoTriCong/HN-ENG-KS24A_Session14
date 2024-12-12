9a21ddb49c5b13cb56204b9c9ec2b67e977fadf3
DoTriCong <tricong5526@gmail.com>
2024-12-12 09:09:00 +0700

    Add files via upload

---
+++ ex5-ss14.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char str[] = "Hoc lap trinh C";
+    int text = 0;
+    int num = 0;
+
+    printf("Chuoi da khai bao: %s\n", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t') {
+            num = 0;
+        } else if (num == 0) {
+          num= 1;
+            text++;
+        }
+    }
+
+    printf("So tu trong chuoi: %d\n", text);
+
+    return 0;
+}
+
